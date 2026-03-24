249979bd7d1a8b28bff9173703b33d1dfd1186ea
Toan <ngosytoan5797dlna@gmail.com>
2026-03-24 22:36:25 +0700

    Add reference folder for string lib

---
+++ Lib/string/reference/string.c
@@ -0,0 +1,18 @@
+#define NULL        ((void *)0)
+
+/**
+ * @brief: This function used to determine length of a string
+ * @param str: pointer to string
+ * @return: Length of string - 0: if NULL
+ */
+unsigned int my_str_len(const char *str)
+{
+    unsigned int len  = 0;
+
+    if (str != NULL)
+    {
+
+    }
+
+    return len;
+}
---
+++ Lib/string/reference/string.h
@@ -0,0 +1,8 @@
+#ifndef __S_T_R_I_N__G____H______
+#define __S_T_R_I_N__G____H______
+
+int a = 5;
+
+unsigned int my_str_len(const char *str);
+
+#endif /* __S_T_R_I_N__G____H______ */

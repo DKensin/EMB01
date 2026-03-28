6133299973dc8fe7a1afb7af385daebc934a3e4d
hyemi <hyemimygame@gmail.com>
2026-03-28 17:53:11 +0700

    Update function descriptions in character.c by HyeMi

--- Lib/character/Hyemi/character/character.c
+++ Lib/character/Hyemi/character/character.c
@@ -1,15 +1,15 @@
+#define     SPACE               (' ')
+#define     NEWLINE             ('\n')
+#define     VERTICAL            ('\v')
+#define     HORIZONTAL_TAB      ('\t')
+
 /**
- *  - my_isalpha                isalpha
-    - my_isdigit                isdigit
-    - my_isspace                isspace
-    - my_isupper                isupper
-    - my_islower                islower
-    - my_toupper                toupper
-    - my_tolower                tolower
+ * @brief: this function is used to check if a character is alphabet or not
+ * @param c: character to check
+ * @return:
+ *  1: c is a alphabet
+ *  0: c is not a alphatbet
  */
-
-#include "character.h"
-
 int my_isalpha(int c)
 {
     if ((c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z'));
@@ -18,26 +18,60 @@ int my_isalpha(int c)
     }
 }
 
+/**
+ * @brief: this function is used to check if a character is digit (0-9)
+ * @param c: character to check
+ * @return:
+ *  1: c is a digit
+ *  0: c is not a digit
+ */
 int my_isdigit(int c)
 {
     return ((c >= '0') && (c <= '9'));
 }
 
+/**
+ * @brief: this function is used to check if a character is white space
+ * @param c: character to check
+ * @return:
+ *  1: c is a white space
+ *  0: c is not a white space
+ */
 int my_isspace(int c)
 {
-    return ((c == ' ') || (c == '\t') || (c == '\n'));
+    return ((c == SPACE) || (c == HORIZONTAL_TAB) || (c == NEWLINE));
 }
 
+/**
+ * @brief: this function is used to check if a character is uppercase (A-Z)
+ * @param c: character to check
+ * @return:
+ *  1: c is a uppercase
+ *  0: c is not a uppercase
+ */
 int my_issupper(int c)
 {
     return ((c >= 'A') && (c <= 'Z'));
 }
 
+/**
+ * @brief: this function is used to check if a character is lowercase (a-z)
+ * @param c: character to check
+ * @return:
+ *  1: c is a lowercase
+ *  0: c is not a lowercase
+ */
 int my_islower(int c)
 {
     return ((c >= 'a') && (c <= 'z'));
 }
 
+/**
+ * @brief: this function is used to convert to uppercase like this a -> A
+ * @param c: character to check
+ * @return:
+ * lowercase or unchanged charracter
+ */
 int my_toupper(int c)
 {
     if ((c >= 'a') && (c <= 'z'))
@@ -47,6 +81,12 @@ int my_toupper(int c)
     return c;
 }
 
+/**
+ * @brief: this function is used to convert to lowercase like this A -> a
+ * @param c: character to check
+ * @return:
+ * uppercase or unchanged charracter
+ */
 int my_tolower(int c)
 {
     if ((c >= 'a') && (c <= 'z'))

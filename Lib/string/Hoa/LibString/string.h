cca20304958961f9dea9f3444cf554a0a2de9be0
Xuân Hoà <xuanhoahn2712@gmail.com>
2026-04-12 17:48:58 +0700

    Update LibStr by Hoa

--- Lib/string/Hoa/LibString/string.h
+++ Lib/string/Hoa/LibString/string.h
@@ -3,6 +3,7 @@
 
 int str_len(char *str);
 int compare_string (char *str1,char *str2);
-void copy_string(char *dest, char *src);
+char *copy_string(char *dest, const char *src)
+char *my_str_concatenate(char *dest, const char *src, unsigned int buf_size);
 int my_atoi(char str[]);
 #endif

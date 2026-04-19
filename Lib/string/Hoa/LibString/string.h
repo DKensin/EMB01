11b7dff72de1377edfd57d54334c6b266bb2f9a0
Xuân Hoà <xuanhoahn2712@gmail.com>
2026-04-19 18:45:01 +0700

    Update string.h

--- Lib/string/Hoa/LibString/string.h
+++ Lib/string/Hoa/LibString/string.h
@@ -4,6 +4,8 @@
 int str_len(char *str);
 int compare_string (char *str1,char *str2);
 char *copy_string(char *dest, const char *src)
-char *my_str_concatenate(char *dest, const char *src, unsigned int buf_size);
+char *str_concatenate(char *dest, const char *src, unsigned int buf_size);
+char *find_a_char(const char *str, const char c);
+char *find_a_str(const char *str, const char *str_f);
 int my_atoi(char str[]);
 #endif
